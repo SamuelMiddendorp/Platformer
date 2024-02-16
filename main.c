ed097d0530533ce573a2a915b8532ac13736054f
Samuel Middendorp <samiezaurus@gmail.com>
2024-02-16 17:28:10 +0100

    Refactor: refactor movement logic into speicifc component

--- main.c
+++ main.c
@@ -1,48 +1,52 @@
 #define BUN2D_IMPLEMENTATION 
 #include <bun2d.h>
 
-
-typedef struct Player{
+typedef struct PhysicsComponent{
     Vec2 pos;
     Vec2 vel;
     Vec2 acc;
     Vec2 dims;
+} PhysicsComponent;
+
+typedef struct Player{
+    PhysicsComponent rigidBody;
 } Player;
+
 int main()
 {
-    bun2dInit(1, 100, 100, 800, 800, "Platformer");
+    bun2dInit(1, 800, 800, 800, 800, "Platformer");
 
     Pixel color = {140,255,255,255};
-    Player player = {{0,0},{0,0},{0,0},{2,5}};
+    Player player = {{{0,0},{0,0},{0,0},{2,5}}};
 
     while (bun2dTick())
     {
         bun2dClear();
         // Input
         if(bun2dKey(KEY_D) == BUN2D_PRESS){
-           player.acc.x += 0.2; 
+           player.rigidBody.acc.x += 0.2; 
         }
 
         if(bun2dKey(KEY_A) == BUN2D_PRESS){
-           player.acc.x -= 0.2; 
+           player.rigidBody.acc.x -= 0.2; 
         }
         if(bun2dKey(KEY_S) == BUN2D_PRESS){
         }
 
         if(bun2dKey(KEY_W) == BUN2D_PRESS){
         }
-        updatePlayer(&player);
-        bun2dRect(player.pos.x, player.pos.y, player.dims.x, player.dims.y, color);
+        updatePhysicsComponent(&player.rigidBody);
+        bun2dRect(player.rigidBody.pos.x, player.rigidBody.pos.y, player.rigidBody.dims.x, player.rigidBody.dims.y, color);
 
     }
 }
-void updatePlayer(Player* player){
-    player->vel.x += player->acc.x;
-    player->vel.y += player->acc.y;
+void updatePhysicsComponent(PhysicsComponent* comp){
+    comp->vel.x += comp->acc.x;
+    comp->vel.y += comp->acc.y;
 
-    player->pos.x += player->vel.x;
-    player->pos.y += player->vel.y;
+    comp->pos.x += comp->vel.x;
+    comp->pos.y += comp->vel.y;
 
-    player->acc.x = 0;
-    player->acc.y = 0; 
+    comp->acc.x = 0;
+    comp->acc.y = 0; 
 }
