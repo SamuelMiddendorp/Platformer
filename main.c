64fe4577227fd377a46f2f443c8317f09bb197ae
Samuel Middendorp <samiezaurus@gmail.com>
2024-02-17 18:52:20 +0100

    Add jumping

--- main.c
+++ main.c
@@ -1,11 +1,13 @@
 #define BUN2D_IMPLEMENTATION 
 #include <bun2d.h>
+#include <stdbool.h>
 
 typedef struct PhysicsComponent{
     Vec2 pos;
     Vec2 vel;
     Vec2 acc;
     Vec2 dims;
+    bool onGround;
 } PhysicsComponent;
 
 typedef struct Player{
@@ -17,7 +19,7 @@ int main()
     bun2dInit(1, 800, 800, 800, 800, "Platformer");
 
     Pixel color = {140,255,255,255};
-    Player player = {{{0,0},{0,0},{0,0},{2,5}}};
+    Player player = {{{0,0},{0,0},{0,0},{2,5},false}};
 
     while (bun2dTick())
     {
@@ -32,9 +34,12 @@ int main()
         }
         if(bun2dKey(KEY_S) == BUN2D_PRESS){
         }
-
-        if(bun2dKey(KEY_W) == BUN2D_PRESS){
+        if(bun2dKey(KEY_W) > 0){
+            if(player.rigidBody.onGround){
+                player.rigidBody.acc.y += 4;
+            }
         }
+        player.rigidBody.acc.y -= 0.1;
         updatePhysicsComponent(&player.rigidBody);
         bun2dRect(player.rigidBody.pos.x, player.rigidBody.pos.y, player.rigidBody.dims.x, player.rigidBody.dims.y, color);
     }
@@ -43,11 +48,14 @@ void updatePhysicsComponent(PhysicsComponent* comp){
     comp->vel.x += comp->acc.x;
     comp->vel.y += comp->acc.y;
 
+    // Gravity
+
     comp->pos.x += comp->vel.x;
     comp->pos.y += comp->vel.y;
 
     comp->acc.x = 0;
     comp->acc.y = 0; 
+
     if(comp->pos.x > 800 - comp->dims.x){
         comp->pos.x = 800 - comp->dims.x;
     }
@@ -55,6 +63,10 @@ void updatePhysicsComponent(PhysicsComponent* comp){
         comp->pos.x = 0;
     }
     if(comp->pos.y < 0){
+        comp->onGround = true;
         comp->pos.y = 0;
+    }
+    else{
+        comp->onGround = false;
     }
 }
